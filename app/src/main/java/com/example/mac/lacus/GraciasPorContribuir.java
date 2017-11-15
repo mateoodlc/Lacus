c745f33efec41ffcad565c5170ee83ade247526d
Mateo <mateoodlc@gmail.com>
2017-11-15 00:07:38 -0500

    Holi (:

--- app/src/main/java/com/example/mac/lacus/GraciasPorContribuir.java
+++ app/src/main/java/com/example/mac/lacus/GraciasPorContribuir.java
@@ -3,6 +3,7 @@ package com.example.mac.lacus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,9 +21,93 @@ public class GraciasPorContribuir extends AppCompatActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gracias_por_contribuir);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         continuar = (Button) findViewById(R.id.continuar);
         categoriaImg = (ImageView) findViewById(R.id.categoriaImg);
         tipoCategoria = (TextView) findViewById(R.id.tipoCaso);
+
+
+        //Strings para recibir los putExtra
+        Intent intent = getIntent();
+        String señalizacionB1 = intent.getStringExtra("señalizacionB1");
+        String infraestructuraB1 = intent.getStringExtra("infraestructuraB1");
+        String culturaB1 = intent.getStringExtra("culturaB1");
+        String seguridadB1 = intent.getStringExtra("seguridadB1");
+
+        String señalizacionB2 = intent.getStringExtra("señalizacionB2");
+        String infraestructuraB2 = intent.getStringExtra("infraestructuraB2");
+        String culturaB2 = intent.getStringExtra("culturaB2");
+        String seguridadB2 = intent.getStringExtra("seguridadB2");
+
+        String señalizacionB3 = intent.getStringExtra("señalizacionB3");
+        String infraestructuraB3 = intent.getStringExtra("infraestructuraB3");
+        String culturaB3 = intent.getStringExtra("culturaB3");
+        String seguridadB3 = intent.getStringExtra("seguridadB3");
+
+
+        //Condicionales para cuando seleccionen el primer botón de cualquier categoría
+        if(señalizacionB1 != null && señalizacionB1.contains("Falta de señalización")){
+            categoriaImg.setImageResource(R.drawable.ic_senales);
+            tipoCategoria.setText("Falta de señalización");
+        } else if(señalizacionB2 != null && señalizacionB2.contains("Señalización errónea")){
+            categoriaImg.setImageResource(R.drawable.ic_senales);
+            tipoCategoria.setText("Señalización errónea");
+        } else if(señalizacionB3 != null && señalizacionB3.contains("No hay separadores viales")){
+            categoriaImg.setImageResource(R.drawable.ic_senales);
+            tipoCategoria.setText("No hay separadores viales");
+        }
+
+        if(infraestructuraB1 != null && infraestructuraB1.contains("Calle en mal estado")){
+            categoriaImg.setImageResource(R.drawable.ic_infraestructura);
+            tipoCategoria.setText("Calle en mal estado");
+
+        } else if(infraestructuraB2 != null && infraestructuraB2.contains("Falta de iluminación")){
+            categoriaImg.setImageResource(R.drawable.ic_infraestructura);
+            tipoCategoria.setText("Falta de iluminación");
+        } else if(infraestructuraB3 != null && infraestructuraB3.contains("Vías de alto riesgo")){
+            categoriaImg.setImageResource(R.drawable.ic_infraestructura);
+            tipoCategoria.setText("Vías de alto riesgo");
+        }
+
+        if(culturaB1 != null && culturaB1.contains("Peatones poco cívicos")){
+            categoriaImg.setImageResource(R.drawable.ic_culture);
+            tipoCategoria.setText("Peatones poco cívicos");
+
+        } else if(culturaB2 != null && culturaB2.contains("Irrespeto por el ciclista")){
+            categoriaImg.setImageResource(R.drawable.ic_culture);
+            tipoCategoria.setText("Irrespeto por el ciclista");
+        } else if(culturaB3 != null && culturaB3.contains("Invasión constante de carril")){
+            categoriaImg.setImageResource(R.drawable.ic_culture);
+            tipoCategoria.setText("Invasión constante de carril");
+        }
+
+        if(seguridadB1 != null && seguridadB1.contains("Alto índice de robo")){
+            categoriaImg.setImageResource(R.drawable.ic_seguridad);
+            tipoCategoria.setText("Alto índice de robo");
+        } else if(seguridadB2 != null && seguridadB2.contains("Locación hostil")){
+            categoriaImg.setImageResource(R.drawable.ic_seguridad);
+            tipoCategoria.setText("Locación hostil");
+        } else if(seguridadB3 != null && seguridadB3.contains("Probabilidad de accidentes")){
+            categoriaImg.setImageResource(R.drawable.ic_seguridad);
+            tipoCategoria.setText("Probabilidad de accidentes");
+        }
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GraciasPorContribuir.this, MapsActivity.class);
+                startActivity(intent);
+                finish();            }
+        });
+
     }
 
     @Override
--- app/src/main/java/com/example/mac/lacus/SeleccionarProblema.java
+++ app/src/main/java/com/example/mac/lacus/SeleccionarProblema.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class SeleccionarProblema extends AppCompatActivity {
+public class SeleccionarProblema extends AppCompatActivity implements View.OnClickListener {
     private String categoria1;
 
     ImageView categoriaImg;
@@ -18,6 +18,10 @@ public class SeleccionarProblema extends AppCompatActivity {
     Button boton2;
     Button boton3;
     Button boton4;
+    private boolean señalizacionB;
+    private boolean infraestructuraB;
+    private boolean culturaB;
+    private boolean seguridadB;
 
 
     @Override
@@ -46,6 +50,9 @@ public class SeleccionarProblema extends AppCompatActivity {
             boton1.setText("Falta de señalizaciones");
             boton2.setText("Señalización errónea");
             boton3.setText("No hay separadores viales");
+            señalizacionB = true;
+        } else{
+            señalizacionB = false;
         }
 
         if(infraestructura != null && infraestructura.contains("infraestructura")){
@@ -55,14 +62,20 @@ public class SeleccionarProblema extends AppCompatActivity {
             boton2.setText("Falta de iluminación");
             boton3.setText("Vías de alto riesgo");
 //            boton4.setText("Hueco o cráter peligroso");
+            infraestructuraB = true;
+        } else{
+            infraestructuraB = false;
         }
 
         if(cultura != null && cultura.contains("cultura")){
             categoriaImg.setImageResource(R.drawable.ic_culture);
             categoriaTxt.setText("Cultura");
-            boton1.setText("Peatones sin cuidado");
+            boton1.setText("Peatones poco cívicos");
             boton2.setText("Irrespeto por el ciclista");
             boton3.setText("Invasión constante de carril");
+            culturaB = true;
+        } else{
+            culturaB = false;
         }
 
         if(seguridad != null && seguridad.contains("seguridad")){
@@ -71,6 +84,9 @@ public class SeleccionarProblema extends AppCompatActivity {
             boton1.setText("Alto índice de robo");
             boton2.setText("Locación hostil");
             boton3.setText("Probabilidad de accidentes");
+            seguridadB = true;
+        } else{
+            seguridadB = false;
         }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -93,6 +109,72 @@ public class SeleccionarProblema extends AppCompatActivity {
 
     public void interpretarCategoria(){
         if(categoria1.contains("cultura")){
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.boton1: /** Start a new Activity MyCards.java */
+                Intent intent = new Intent(this, GraciasPorContribuir.class);
+                if(señalizacionB == true){
+                    intent.putExtra("señalizacionB1", "Falta de señalización");
+                }
+                if(culturaB == true) {
+                    intent.putExtra("culturaB1", "Peatones poco cívicos");
+                }
+                if(seguridadB == true){
+                    intent.putExtra("seguridadB1", "Alto índice de robo");
+                }
+                if(infraestructuraB == true){
+                    intent.putExtra("infraestructuraB1", "Calle en mal estado");
+                }
+
+                this.startActivity(intent);
+                break;
+
+            case R.id.boton2: /** Start a new Activity MyCards.java */
+                Intent intent2 = new Intent(SeleccionarProblema.this, GraciasPorContribuir.class);
+
+                if(señalizacionB == true){
+                    intent2.putExtra("señalizacionB2", "Señalización errónea");
+                }
+                if(culturaB == true) {
+                    intent2.putExtra("culturaB2", "Irrespeto por el ciclista");
+                }
+                if(seguridadB == true){
+                    intent2.putExtra("seguridadB2", "Locación hostil");
+                }
+                if(infraestructuraB == true){
+                    intent2.putExtra("infraestructuraB2", "Falta de iluminación");
+                }
+                startActivity(intent2);
+                break;
+
+            case R.id.boton3: /** Start a new Activity MyCards.java */
+                Intent intent3 = new Intent(SeleccionarProblema.this, GraciasPorContribuir.class);
+
+                System.out.println("entró");
+                if(señalizacionB == true){
+                    intent3.putExtra("señalizacionB3", "No hay separadores viales");
+                }
+                if(culturaB == true) {
+                    intent3.putExtra("culturaB3", "Invasión constante de carril");
+                }
+                if(seguridadB == true){
+                    intent3.putExtra("seguridadB3", "Probabilidad de accidentes");
+                }
+                if(infraestructuraB == true){
+                    intent3.putExtra("infraestructuraB3", "Vías de alto riesgo");
+                }
+                startActivity(intent3);
+                break;
+
+           /* case R.id.boton4: /** Start a new Activity MyCards.java */
+           //     Intent intent4 = new Intent(SeleccionarProblema.this, SeleccionarProblema.class);
+             //   intent4.putExtra("seguridadID", "seguridad");
+               // startActivity(intent4);
+                //break;
         }
     }
 }
