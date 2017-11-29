118c4f4d140fb30272fc7c21a5f642d3cef1a678
Mateo <mateoodlc@gmail.com>
2017-11-29 13:30:08 -0500

    animación de marcador

--- app/src/main/java/com/example/mac/lacus/GestionInformacion.java
+++ app/src/main/java/com/example/mac/lacus/GestionInformacion.java
@@ -1,14 +1,18 @@
 package com.example.mac.lacus;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class GestionInformacion extends AppCompatActivity {
 
     Toolbar toolbar;
+    ImageView marcador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,14 @@ public class GestionInformacion extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ///////////////////////////////////////////////////////////////
+        //set del vector animado
+        marcador = (ImageView) findViewById(R.id.marcadoranimado);
+        Drawable drawable = marcador.getDrawable();
+        if(drawable instanceof Animatable){
+            ((Animatable) drawable).start();
+        }
 
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
