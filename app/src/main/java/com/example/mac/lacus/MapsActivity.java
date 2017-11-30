01419584f1e11963811dcb18f976f7e3dfb6fd35
JFCP <jufe94@hotmail.com>
2017-11-30 01:08:29 -0500

    30/11/2017 1:08 am
    Se borran los parámetros basura.

--- app/src/main/java/com/example/mac/lacus/MapsActivity.java
+++ app/src/main/java/com/example/mac/lacus/MapsActivity.java
@@ -115,25 +115,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private FMapa.OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FMapa.
-     */
-    // TODO: Rename and change types and number of parameters
- /*   public static MapsActivity newInstance(String param1, String param2) {
-        FMapa fragment = new FMapa();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-*/
-
     public static MapsActivity newInstance(String s) {
         MapsActivity fragment = new MapsActivity();
         // Set the arguments.
